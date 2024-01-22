1ded3eeaa59bd9169b65957aa8bf6b75343e35e1
mytest31 <mytest.zoomcamp1@gmail.com>
2024-01-22 22:13:31 +0300

    refactor: decompose task code into functions

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -14,12 +14,70 @@ const READDIR_OPTIONS = {
   withFileTypes: true,
 };
 
-function copyAllFiles(
+/**
+ * copy the file from source to destination folder
+ * @param {*} absoluteSourcePath - the absolute path to source folder
+ * @param {*} absoluteDestinationPath - the absolute path to destination folder
+ * @param {*} file - the file object of fs.Dirent class
+ */
+function copyOneFile(absoluteSourcePath, absoluteDestinationPath, file) {
+  fs.copyFile(
+    path.join(absoluteSourcePath, file.name),
+    path.join(absoluteDestinationPath, file.name),
+    (err) => {
+      if (err) return console.error(err.message);
+      console.log(
+        `The file ${file.name}` +
+          ' was copied from source to destination folder',
+      );
+    },
+  );
+}
+
+/**
+ * update the modified file from source to destination folder
+ * @param {*} absoluteSourcePath - the absolute path to source folder
+ * @param {*} absoluteDestinationPath - the absolute path to destination folder
+ * @param {*} file - the file object of fs.Dirent class
+ * @param {*} statsDestination - stats info about file in the destination folder
+ */
+function updateOneFile(
   absoluteSourcePath,
   absoluteDestinationPath,
-  readdir_option,
+  file,
+  statsDestination,
 ) {
-  fs.readdir(absoluteSourcePath, readdir_option, (error, dirContent) => {
+  fs.stat(path.join(file.path, file.name), (error, statsSource) => {
+    if (error) return console.error(error.message);
+    const sourceModificationTime = statsSource.atimeMs;
+    const destinationModificationTime = statsDestination.atimeMs;
+    if (sourceModificationTime > destinationModificationTime) {
+      fs.copyFile(
+        path.join(absoluteSourcePath, file.name),
+        path.join(absoluteDestinationPath, file.name),
+        (err) => {
+          if (err) return console.error(err.message);
+          console.log(
+            `The file ${file.name}` + ' was updated in the destination folder',
+          );
+        },
+      );
+    }
+  });
+}
+
+/**
+ * copy and update files from source to destination folder
+ * @param {*} absoluteSourcePath - the absolute path to source folder
+ * @param {*} absoluteDestinationPath - the absolute path to destination folder
+ * @param {*} readdir_options - option of readdir
+ */
+function copyAndUpdateFiles(
+  absoluteSourcePath,
+  absoluteDestinationPath,
+  readdir_options,
+) {
+  fs.readdir(absoluteSourcePath, readdir_options, (error, dirContent) => {
     if (error) return console.error(error.message);
     const files = dirContent.filter((file) => file.isFile());
     files.forEach((file) => {
@@ -28,45 +86,37 @@ function copyAllFiles(
         (error, statsDestination) => {
           if (error) {
             // there is no file in the destination folder
-            fs.copyFile(
-              path.join(absoluteSourcePath, file.name),
-              path.join(absoluteDestinationPath, file.name),
-              (err) => {
-                if (err) return console.error(err.message);
-                console.log(
-                  `The file ${file.name}` +
-                    ' was copied from source to destination folder',
-                );
-              },
-            );
+            copyOneFile(absoluteSourcePath, absoluteDestinationPath, file);
           } else {
-            // copy modified files
-            fs.stat(path.join(file.path, file.name), (error, statsSource) => {
-              if (error) return console.error(error.message);
-              const sourceModificationTime = statsSource.atimeMs;
-              const destinationModificationTime = statsDestination.atimeMs;
-              if (sourceModificationTime > destinationModificationTime) {
-                fs.copyFile(
-                  path.join(absoluteSourcePath, file.name),
-                  path.join(absoluteDestinationPath, file.name),
-                  (err) => {
-                    if (err) return console.error(err.message);
-                    console.log(
-                      `The file ${file.name}` +
-                        ' was updated in the destination folder',
-                    );
-                  },
-                );
-              }
-            });
+            updateOneFile(
+              absoluteSourcePath,
+              absoluteDestinationPath,
+              file,
+              statsDestination,
+            );
           }
         },
       );
     });
   });
+}
+
+/**
+ * remove files that were deleted in the source folder
+ * @param {*} absoluteSourcePath - the absolute path to source folder
+ * @param {*} absoluteDestinationPath - the absolute path to destination folder
+ * @param {*} readdir_option
+ */
+function removeInvalidFiles(
+  absoluteSourcePath,
+  absoluteDestinationPath,
+  readdir_option,
+) {
   fs.readdir(absoluteDestinationPath, readdir_option, (error, dirContent) => {
     if (error) return console.error(error.message);
+
     const files = dirContent.filter((file) => file.isFile());
+
     files.forEach((file) => {
       fs.stat(path.join(absoluteSourcePath, file.name), (error) => {
         if (error) {
@@ -84,15 +134,40 @@ function copyAllFiles(
   });
 }
 
-function copyDirectory(
+function processAllFiles(
   absoluteSourcePath,
   absoluteDestinationPath,
-  readdir_option,
+  readdir_options,
+) {
+  removeInvalidFiles(
+    absoluteSourcePath,
+    absoluteDestinationPath,
+    readdir_options,
+  );
+  copyAndUpdateFiles(
+    absoluteSourcePath,
+    absoluteDestinationPath,
+    readdir_options,
+  );
+}
+
+function copyDirectoryFiles(
+  absoluteSourcePath,
+  absoluteDestinationPath,
+  readdir_options,
 ) {
   fs.mkdir(ABSOLUTE_DESTINATION_PATH, MKDIR_OPTIONS, (error) => {
     if (error) return console.error(error.message);
-    copyAllFiles(absoluteSourcePath, absoluteDestinationPath, readdir_option);
+    processAllFiles(
+      absoluteSourcePath,
+      absoluteDestinationPath,
+      readdir_options,
+    );
   });
 }
 
-copyDirectory(ABSOLUTE_SOURCE_PATH, ABSOLUTE_DESTINATION_PATH, READDIR_OPTIONS);
+copyDirectoryFiles(
+  ABSOLUTE_SOURCE_PATH,
+  ABSOLUTE_DESTINATION_PATH,
+  READDIR_OPTIONS,
+);
